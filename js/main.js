11020491b13534c921c6854b946a5431100e16f0
Lowell Bander <lowellbander@gmail.com>
2016-04-13 12:36:48 -0700

    added in react-canvas

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-//var ReactCanvas = require('react-canvas');
+var ReactCanvas = require('react-canvas');
 
 var App = React.createClass({
     render: function () {
