526cfe72bacfa4bff1d3fe7292f10e79dfc755c7
yan <ryant_mac@hotmail.com>
2016-04-19 21:40:07 -0700

    Add modified node_modules/react-canvas/*

---
+++ node_modules/react-canvas/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  cache: true,
+
+  watch: true,
+
+  entry: {
+    'listview': ['./examples/listview/app.js'],
+    'timeline': ['./examples/timeline/app.js'],
+    'gradient': ['./examples/gradient/app.js'],
+    'css-layout': ['./examples/css-layout/app.js']
+  },
+
+  output: {
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'jsx-loader!transform/cacheable?envify' },
+    ],
+    postLoaders: [
+      { loader: "transform?brfs" }
+    ]
+  },
+
+  resolve: {
+    root: __dirname,
+    alias: {
+      'react-canvas': 'lib/ReactCanvas.js'
+    }
+  }
+};
