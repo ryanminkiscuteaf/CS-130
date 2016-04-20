33678a1e23357e85bad387bded9d61104ffb36ea
Lowell Bander <lowellbander@gmail.com>
2016-04-20 15:59:28 -0700

    pseudo, save

--- pseudo.js
+++ pseudo.js
@@ -35,10 +35,22 @@ var StructuredObject = React.createClass({
 // a reference to the event handler will be passed to objects that need a handle to it
 
 DRAG_EVENT = 'shapeDragged';
+SAVE_EVENT = 'saveClicked';
 
 var AnObjectInPartsBin = React.createClass({
     onDrag: function () {
         this.props.eventHandler.publish(DRAG_EVENT, this.state);
+    }
+});
+
+var SaveButton = React.createClass({
+    onClick: function () {
+        this.props.eventHandler.publish(SAVE_EVENT, null);
+    },
+    render: function () {
+        return (
+            <p>Save current object</p>
+        );
     }
 });
 
@@ -47,6 +59,18 @@ var TheGlobalScene = React.createClass({
         this.props.eventHandler.subscribe(DRAG_EVENT, function (e) {
             // TODO: validate shape structure
             this.setState({newShapes: this.state.newShapes.concat([e])});
+        });
+        this.props.eventHandler.subscribe(SAVE_EVENT, function (e) {
+            var compose = function (shapes) {return {};};
+            var newObj = compose(this.state.newShapes);
+            this.setState(
+                {objects: this.state.objects.concat([newObj])},
+                function () {
+                    // clear the workspace 
+                    this.setState({newShapes: []});
+                }
+            );
+            // TODO replace workspace with the object you were constructing
         });
         return {
             objects: [],
