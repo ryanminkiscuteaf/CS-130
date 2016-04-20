9345d750cb4ee9ad3f60a353c9395707eeca482e
Lowell Bander <lowellbander@gmail.com>
2016-04-20 15:34:36 -0700

    added pseudo structure

---
+++ pseudo.js
@@ -0,0 +1,14 @@
+// representation of a structured object
+
+<group>
+    <representation>
+        <Circle props={...}/>
+        <Square props={...}/>
+        <Square props={...}/>
+    </representation>
+    <anchors>
+        <Anchor />
+        <Anchor />
+        <Anchor />
+    </anchors>
+</group>
