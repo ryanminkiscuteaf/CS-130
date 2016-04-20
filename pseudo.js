2c312d098042e4d1b2f34e45974ad44980e8268d
Lowell Bander <lowellbander@gmail.com>
2016-04-20 15:43:43 -0700

    added more pseudocode

--- pseudo.js
+++ pseudo.js
@@ -12,3 +12,40 @@
         <Anchor />
     </anchors>
 </group>
+
+
+var StructuredObject = React.createClass({
+    getInitialState: function () {
+        return {
+            shapes: [],
+            anchors: []
+        };
+    },
+    render: function () {
+        return (
+            <Group>
+                {this.state.shapes}
+                {this.state.anchors}
+            </Group>
+        );
+    }
+});
+
+// we'll us a publish/subscribe pattern to handle events
+// a reference to the event handler will be passed to objects that need a handle to it
+
+DRAG_EVENT = 'shapeDragged';
+
+var AnObjectInPartsBin = React.createClass({
+    onDrag: function () {
+        this.props.eventHandler.publish(DRAG_EVENT, this.state);
+    }
+});
+
+var TheGlobalScene = React.createClass({
+    getInitialState: function () {
+        this.props.eventHandler.subscribe(DRAG_EVENT, function (e) {
+            // apend the new object to the current scene
+        })
+    }
+});
