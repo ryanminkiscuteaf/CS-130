29277ed2ec2047c28f5bf2dd62e25a68df81215b
yan <ryant_mac@hotmail.com>
2016-05-03 09:28:26 -0700

    Integrate PartsBin with generic objects

--- src/js/Conjurer.js
+++ src/js/Conjurer.js
@@ -6,27 +6,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactCanvas from 'react-canvas';
 
-import Test from './Test';
 import Draggable from './Draggable';
-import DraggableCircle from './DraggableCircle';
-import Rectangle from './shapes/Rectangle';
 import Generic from './Generic';
+import PartsBin from './PartsBin';
 
 let Surface = ReactCanvas.Surface;
 let Group = ReactCanvas.Group;
 let Image = ReactCanvas.Image;
 let Text = ReactCanvas.Text;
 
-var EventEmitter = require('wolfy87-eventemitter');
-var ee = new EventEmitter();
+let Event = require('./event/EventNames');
+let ee = require('./event/EventEmitter');
+let TEST_EVENT = "event handler test";
 
-var TEST_EVENT = "event handler test";
+let sampleItems = require('./SampleItems');
 
 class Conjurer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
     ee.addListener(TEST_EVENT, function() {console.log('event handler seems to be working');});
+
+    // Listener for event from the Parts Bin
+    ee.addListener(Event.PARTS_BIN_CLONE_ITEM_EVENT, this.cloneItem.bind(this));
 
     // coordinate data for drawing shapes
     this.x_orig = 0;
@@ -40,7 +42,8 @@ class Conjurer extends React.Component {
   componentWillMount() {
     this.state = {
       isDrawing: false,
-      objects: []
+      objects: [],
+      clone: null
     };
   }
 
@@ -49,7 +52,10 @@ class Conjurer extends React.Component {
     this.x_orig = e.clientX;
     this.y_orig = e.clientY;
 
-    this.setState({
+    // Emit an event to parts bin to add a new item
+    ee.emitEvent(Event.PARTS_BIN_ADD_ITEM_EVENT, [this.getSampleGeneric()]);
+
+    /*this.setState({
       objects: this.state.objects.concat(
         {
           id: this.dragref,
@@ -77,7 +83,42 @@ class Conjurer extends React.Component {
       )
     });
 
-    this.dragref++;
+    this.dragref++;*/
+  }
+
+  getPartsBinStyle() {
+    return {
+      top: 0,
+      left: 0,
+      width: 180,
+      height: window.innerHeight,
+      backgroundColor: 'red',
+      itemHeight: 180,
+      itemPadding: 10
+    };
+  }
+
+  getSampleGeneric() {
+    var id = Math.floor((Math.random() * 1000) + 1);
+    return {
+      id: id,
+      ref: id,
+      shapes: [{
+            type: 'circle',
+            top: 0, //10
+            left: 0, //10
+            width: 50,
+            height: 50
+          },
+          {
+            type: 'circle',
+            top: 150,
+            left: 50,
+            width: 60,
+            height: 60
+          }
+          ]
+    };
   }
 
   getTextStyle() {
@@ -94,29 +135,39 @@ class Conjurer extends React.Component {
   getWrapperStyle() {
     return {
       top: 0,
-      left: 0,
+      left: 200,
       width: window.innerWidth,
       height: window.innerHeight
     };
+  }
+
+  // Clone parts bin item given an emitted event
+  cloneItem(item) {
+    console.log("Clone item conjurer - id: " + item.id);
+
+    item.id = this.dragref;
+    item.ref = this.dragref;
+    item.x = window.innerWidth/2;
+    item.y = window.innerHeight/2;
+
+    this.setState({
+      objects: this.state.objects.concat(item)
+    });
+
+    this.dragref++;
   }
 
   render() {
     var surfaceWidth = window.innerWidth;
     var surfaceHeight = window.innerHeight;
     var textStyle = this.getTextStyle();
-
+    
     ee.emitEvent(TEST_EVENT);
 
     return (
       <Surface width={surfaceWidth} height={surfaceHeight} left={0} top={0}>
+        <PartsBin style={this.getPartsBinStyle()} items={sampleItems} />
         <Group style={this.getWrapperStyle()} onMouseDown={this.handleMouseDown.bind(this)}>
-          <Text style={textStyle}>
-            Here is some text.
-          </Text>
-          <Draggable xCoord={50} yCoord={150}>
-            <DraggableCircle />
-          </Draggable>
-          <Rectangle style={{top: 200, left: 300, width: 100, height: 200, borderWidth: 5}}/>
           {this.state.objects.map(function(obj) {
             return (
               <Draggable xCoord={obj.x} yCoord={obj.y}>
@@ -124,9 +175,8 @@ class Conjurer extends React.Component {
                   key={obj.id}
                   width={obj.width}
                   height={obj.height}
-                  shapes={obj.shapes}
-                />
-              </Draggable>
+                  shapes={obj.shapes} />
+              </Draggable>          
             );
           })}
         </Group>
