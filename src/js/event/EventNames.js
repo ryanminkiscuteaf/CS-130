c4c5cb9a3f6a8f732dea805f1c167775a030e262
yan <ryant_mac@hotmail.com>
2016-05-15 14:48:08 -0700

    Add Events for Code Editor

--- src/js/event/EventNames.js
+++ src/js/event/EventNames.js
@@ -2,5 +2,7 @@ module.exports = {
 	PARTS_BIN_ADD_ITEM_EVENT: "parts bin add item event",
 	PARTS_BIN_REMOVE_ITEM_EVENT: "parts bin remove item event",
 	PARTS_BIN_REPLACE_ITEM_EVENT: "parts bin replace item event",
-	PARTS_BIN_CLONE_ITEM_EVENT: "parts bin clone item event"
+	PARTS_BIN_CLONE_ITEM_EVENT: "parts bin clone item event",
+	CODE_EDITOR_ON_CLICK: "code editor on click",
+	CODE_EDITOR_OFF_CLICK: "code editor off click"
 };
